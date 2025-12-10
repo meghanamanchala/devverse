b5f16ac5c06ef08548469fd3ce2fbc0b446e7590
Meghana <meghanamanchala@gmail.com>
2025-12-10 09:53:09 +0530

    feat: refactor socket connection to use environment variable for URL

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -3,15 +3,18 @@ import { useEffect, useRef } from "react";
 import io from "socket.io-client";
 import { useSelector } from "react-redux";
 
+
 let socketInstance = null;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 export const useSocket = () => {
 	const socketRef = useRef();
 	const user = useSelector((state) => state.auth.user);
 
+
 	useEffect(() => {
 		if (!socketInstance) {
-			socketInstance = io("http://localhost:5000");
+			socketInstance = io(SOCKET_URL);
 		}
 		socketRef.current = socketInstance;
 		// Join the user's room for private messaging
